209c98884db00c1c9854fefc4d48c9b538340ac1
jprashant28 <jprashant28@users.noreply.github.com>
2016-05-21 05:29:00 -0500

    Create clpost.js

---
+++ www/clpost.js
@@ -0,0 +1,13 @@
+var cluttr =  {
+    postItem: function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'CLPostPlugin', // mapped to our native Java class called "CLPostPlugin"
+            'POST2CL', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+            }]
+        );
+    }
+}
+module.exports = cluttr;
