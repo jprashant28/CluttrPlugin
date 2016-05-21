adc8b8017efed39aa765aa400bb1c3bfc20cdd15
jprashant28 <jprashant28@users.noreply.github.com>
2016-05-21 05:23:01 -0500

    Create CLPostPlugin.java

---
+++ src/android/CLPostPlugin.java
@@ -0,0 +1,33 @@
+package com.cluttr.post;
+ 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import org.apache.cordova.api.Plugin;
+import com.cluttr.post.LoginHttpUnit;
+
+public class CLPostPlugin extends CordovaPlugin {
+
+	@Override
+    public boolean execute( String action, JSONArray args, CallbackContext callbackContext) throws JSONException
+	{
+		try
+		{
+			if (action.equals("POST2CL"))
+			{
+				LoginHttpUnit.post();
+				callbackContext.success();
+				return true;
+			}
+			return true;
+		}
+		catch(Exception e)
+		{
+			callbackContext.error(e.getMessage());
+			return false;
+		}
+	}
+}
