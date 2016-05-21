30a88aa4d113f561fef1cb56845632cf5904123d
jprashant28 <jprashant28@users.noreply.github.com>
2016-05-21 12:44:32 -0500

    Removed Item Class - moving to its own file

--- src/android/LoginHttpUnit.java
+++ src/android/LoginHttpUnit.java
@@ -108,20 +108,3 @@ public class LoginHttpUnit {
 		System.out.println(resp.getText());
 	}
 }
-
-class Item {
-	public String 
-		title = "Original 1965 Piston Cup",
-		asking = "500",
-		location = "Radiator Springs in Carburetor County",
-		postal_code = "56666",
-		description = "A Piston Cup championship consists of various races where the racers are dotted with an specific number of points according to their positions on each race during the championship."
-		;
-	
-	//commenting the images so that it works with phongap for now.
-	public String[] imageurls = {
-                                  //"http://lh3.googleusercontent.com/_zhGQqZy6xK3mzsEeqIJb-9hV7y435-VP2qJmwpyV6XAdmjfCYroKpXxO28XuzcHUtxefkkvVBJPoson7qmJgA=s1024",
-                                  "http://img12.shop-pro.jp/PA01065/037/product/59533789.jpg?20130524194316",
-                                  "http://vignette1.wikia.nocookie.net/pixarcars/images/6/6e/From_This_Movie_Cars.jpg/revision/latest?cb=20140206225406"
-								};
-}
