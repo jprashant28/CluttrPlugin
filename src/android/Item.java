ad0a191868cbbd8a6f7bd56300f4c306a8606f30
jprashant28 <jprashant28@users.noreply.github.com>
2016-05-22 14:17:53 -0500

    Added default constructor

--- src/android/Item.java
+++ src/android/Item.java
@@ -9,6 +9,10 @@ public class Item {
 		description = "A Piston Cup championship consists of various races where the racers are dotted with an specific number of points according to their positions on each race during the championship."
 		;
 	
+	public Item()
+	{
+	}
+	
 	//commenting the images so that it works with phongap for now.
 	public String[] imageurls = {
                                   //"http://lh3.googleusercontent.com/_zhGQqZy6xK3mzsEeqIJb-9hV7y435-VP2qJmwpyV6XAdmjfCYroKpXxO28XuzcHUtxefkkvVBJPoson7qmJgA=s1024",
